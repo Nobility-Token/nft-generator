0432d9a31d7485806a4bc0be6b579b5b7a25cd00
rmcmk <ryley.kimmel@live.com>
2022-02-09 20:19:54 -0500

    nft-generator: generate rarity of components from generated collection

--- src/main/java/com/patrity/Attribute.java
+++ src/main/java/com/patrity/Attribute.java
@@ -1,16 +1,22 @@
 package com.patrity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.File;
 
 @Data
+@AllArgsConstructor
 public class Attribute {
 
-    public final AttributeType type;
-    public final String name;
+    public AttributeType type;
+    public String name;
     @JsonIgnore
-    public final File file;
-    public final Rarity rarity;
+    public File file;
+    public Rarity rarity;
+
+    // Default no-arg constructor required for jackson
+    public Attribute() {
+    }
 }
